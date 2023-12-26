81c2acc1215cd99afb03221c0c095c076932bb6f
Grant Keller <grant@kellerdigital.com>
2023-12-26 06:06:09 -0800

    Added hero bg image, moved hero message into layout, added gradient, added footer, tightened up

--- davant-systems/src/pages/index.tsx
+++ davant-systems/src/pages/index.tsx
@@ -8,16 +8,15 @@ const IndexPage: React.FC<PageProps> = () => {
 		<>
 			<main className="z-20">
 				<section className="hero
-					relative
-					block">
+					relative">
 					{/* <div className="hero-overlay bg-opacity-60"></div> */}
 					<div className="hero-content
 						relative
 						block
 						z-20
-						text-center text-neutral-content py-32">
+						text-center text-neutral-content py-12 md:py-20">
 						<div className="hero-content__inner
-							flex flex-col items-center">
+							flex flex-col items-center mx-auto">
 							<div className="logo">
 								<h1 className="logo__word--first
 										relative
@@ -27,10 +26,10 @@ const IndexPage: React.FC<PageProps> = () => {
 										sm:pt-1
 										md:pt-1
 										lg:pt-1
-										mb-10
-										sm:mb-10
-										md:mb-20
-										lg:mb-30
+										mb-32
+										sm:mb-32
+										md:mb-24
+										lg:mb-28
 									">
 									<span className="
 										font-orbitron
@@ -84,32 +83,46 @@ const IndexPage: React.FC<PageProps> = () => {
 								hero-content__content
 								max-w-4xl
 								mx-2">
-								<p className="
-									max-w-sm
-									md:max-w-md
-									lg:max-w-xl
-									mx-auto
-									text-cyan-200
-									mb-44
-									text-lg
-									sm:text-lg
-									md:text-1xl
-									lg:text-2xl
-									bg-gradient-to-br
-									from-cyan-200
-									to-cyan-400
-									bg-clip-text
-									text-transparent
-								">Real-time AI image tools for next-gen creative workflows & mind-blowing event experiences</p>
 
-								<div className="md:flex md:gap-4">
-									<div className="join join-vertical px-8 pt-10 pb-4 mb-auto mx-auto bg-black bg-opacity-50">
-										<div className="">
-											<p className="text-cyan-200 text-xs mb-3">Beta available until 2/17/24</p>
-										</div>
-										<a href="/beta">
-											<button
-												className="btn btn-primary text-lg mb-2 indicator
+								<div className="flex flex-col-reverse justify-center md:flex-row items-center md:gap-10">
+
+									<div>
+										<p className="
+										max-w-sm
+										text-left
+										md:max-w-md
+										lg:max-w-xl
+										mx-auto
+										text-cyan-200
+										mb-2
+										md:mb-12
+										text-lg
+										sm:text-2g
+										md:text-3xl
+										lg:text-5xl
+										w-auto
+										bg-gradient-to-br
+										from-cyan-500
+										via-secondary
+										to-purple-500
+										bg-clip-text
+										text-transparent
+										block
+										position
+										overflow-visible
+									">
+										<span className="pt-2 text-6xl font-bold block leading-10 lg:mb-1">Next-Gen </span>
+										<span className="block font-light leading-10 pl-0.5 mb-4">creative tools</span>
+										<span className="block text-5xl font-bold leading-10 lg:mb-3">Mind-blowing </span>
+										<span className="block font-light leading-3 pl-0.5 pb-8 -mb-4 overflow-visible">imagination</span></p>
+
+										<div className="join join-vertical px-8 pt-10 pb-4 -mt-12 md:mt-auto z-10 mb-auto mx-auto bg-base-300 bg-opacity-80 shadow-md">
+											<div className="">
+												<p className="text-cyan-200 text-xs mb-3">Beta available until 2/17/24</p>
+											</div>
+											<a href="/beta">
+												<button
+													className="btn btn-primary text-lg mb-2 indicator
 												bg-gradient-to-br
 												from-pink-400
 												via-fuchsia-500
@@ -122,26 +135,27 @@ const IndexPage: React.FC<PageProps> = () => {
 												border-cyan-500 rounded-sm
 												border-opacity-50 shadow-pink-500 shadow-sm
 												">
-												<span className="indicator-item indicator-bottom badge badge-success border-1 border-green-200 border-opacity-50 shadow-success shadow-sm">Free Access</span>
-												Get Davant Studio
-												{/* <div className="badge badge-success">Free!</div> */}
-											</button>
-										</a>
+													<span className="indicator-item indicator-bottom badge badge-success border-1 border-green-200 border-opacity-50 shadow-success shadow-sm">Free Access</span>
+													Get Davant Studio
+													{/* <div className="badge badge-success">Free!</div> */}
+												</button>
+											</a>
 
-										<div className="alert w-auto bg-transparent text-xs grid-flow-col text-left px-0">
-											<svg height="22px" width="24px" className="stroke-white fill-cyan-300" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 452.986 452.986" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M165.265,53.107L21.689,81.753v132.531l143.575-2.416V53.107 M431.297,245.583l-233.18-3.991 v164.822l233.18,46.571V245.583 M165.265,241.097l-143.575-2.438v132.509l143.575,28.668V241.097 M431.297,0l-233.18,46.528 v164.822l233.18-3.969V0"></path> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g></svg>
-											{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
-											<div className="text-cyan-100">
-												<p className="font-bold">Recommended specs:</p>
-												<p>Windows 10/11 | 16gb RAM</p>
-												<p>Nvidia 2000 series | AMD 6000 series</p>
+											<div className="alert border-0 w-auto bg-transparent text-xs grid-flow-col text-left px-0">
+												<svg height="22px" width="24px" className="stroke-white fill-cyan-300" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 452.986 452.986" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M165.265,53.107L21.689,81.753v132.531l143.575-2.416V53.107 M431.297,245.583l-233.18-3.991 v164.822l233.18,46.571V245.583 M165.265,241.097l-143.575-2.438v132.509l143.575,28.668V241.097 M431.297,0l-233.18,46.528 v164.822l233.18-3.969V0"></path> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g></svg>
+												{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
+												<div className="text-cyan-100">
+													<p className="font-bold">Recommended specs:</p>
+													<p>Windows 10/11 | 16gb RAM</p>
+													<p>Nvidia 2000 series | AMD 6000 series</p>
+												</div>
 											</div>
 										</div>
 									</div>
 
-									<div className="max-w-sm shadow-xl border-2 border-black border-opacity-5 bg-black bg-opacity-40">
+									<div className="max-w-sm shadow-lg border-2 border-primary border-opacity-5 bg-black bg-opacity-40">
 										<StaticImage
-											src="../images/davant-studio-presets.png"
+											src="../images/davant-studio-controls.png"
 											alt="Screenshot of Davant Studio software"
 										/>
 									</div>
@@ -156,31 +170,37 @@ const IndexPage: React.FC<PageProps> = () => {
 						left-1/2
 						-translate-x-1/2
 						min-w-full
-						z-0
+						-z-10
+						opacity-50
+						
 						h-full">
 						<StaticImage
-							className="
+							className="hero__background-image
 							h-full
 							relative
 							block"
 							src="../images/hero1.png"
+							quality={100}
 							placeholder="blurred"
+							layout="fullWidth"
 							loading="eager"
 							objectFit="cover"
 							objectPosition="center"
-							alt="Screenshot of Davant Studio software"/>
+							alt="Screenshot of Davant Studio software" />
 					</div>
 				</section>
 
 				<div className="
 				relative
-				pb-44
+				pb-60
+				-mt-60
 				bg-gradient-to-b
 				from-transparent
-				to-black
-				opacity-30
+				to-base-300
+				opacity-100
 				mb-0
 				"></div>
+				<div className="bg-base-300 pb-32"></div>
 
 				<section className="relative">
 					<div className="
@@ -195,7 +215,7 @@ const IndexPage: React.FC<PageProps> = () => {
 					lg:max-w-5xl
 					min-h-screen
 					px-5
-					pt-20
+					pt-28
 					pb-44
 					z-10
 					relative
@@ -325,6 +345,19 @@ const IndexPage: React.FC<PageProps> = () => {
 					</div>
 				</section>
 			</main>
+			<footer>
+				<section className="max-w-4xl mx-auto block w-full
+					px-4
+					py-8">
+				<a
+					className="flex items-center text-xs text-cyan-300"
+					href="https://instagram.com/DavantSystems">
+					<svg className="stroke-secondary fill-cyan-700 mr-2"
+						width="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"></path> <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"></path> </g></svg>
+					@DavantSystems
+				</a>
+				</section>
+			</footer>
 			<div className="vhs-effect z-0 absolute"></div>
 			<div className="vhs-effect-vert z-0 absolute"></div>
 		</>
