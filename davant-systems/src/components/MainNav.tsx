75bba6d7d44cbe76eb61c1e8878bfdd8bcb48a53
Grant Keller <grant@kellerdigital.com>
2024-05-21 01:46:44 -0700

    Fix mobile menu training item

--- davant-systems/src/components/MainNav.tsx
+++ davant-systems/src/components/MainNav.tsx
@@ -10,16 +10,16 @@ const MainNav = () => {
   const contentVariables = useContext(ContentContext);
 
   return (
-    <div className="fixed top-0 z-50 navbar bg-gradient-to-b from-base-100 to-transparent bg-opacity-80">
+    <div className="fixed top-0 z-50 bg-opacity-0 navbar bg-gradient-to-b from-black to-transparent">
       <div className="navbar-start">
         <Link className="text-xl btn btn-ghost" to="/">
           <StaticImage src="../images/DavantSystems-logo-1000.png" quality={100} alt="Davant Systems" placeholder="blurred" layout="fixed" width={100} height={40} />
         </Link>
       </div>
       <div className="hidden navbar-center md:flex">
-        <ul className="px-1 tracking-tight menu menu-horizontal text-cyan-100">
-          <li><Link to="/davant-studio/">Davant Studio</Link></li>
-          <li><Link to="/photobooth/">Magic Mirror Photo Booth</Link></li>
+        <ul className="px-1 tracking-tight menu menu-horizontal text-cyan-100 drop-shadow-sm">
+          <li><Link to="/davant-studio/" className="hover:bg-base-100 hover:bg-opacity-70">Davant Studio</Link></li>
+          <li><Link to="/photobooth/">Magic Mirror AI Photo Booth</Link></li>
           <li><Link to="/training/">Training & Workshops</Link></li>
         </ul>
       </div>
@@ -32,12 +32,12 @@ const MainNav = () => {
         </div>
         <ul tabIndex={0} className="menu menu-sm dropdown-content text-cyan-100 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 right-0">
           <li><Link to="/davant-studio/">Davant Studio</Link></li>
-          <li><Link to="/photobooth/">Magic Mirror Photo Booth</Link></li>
-          <li><Link to="/consulting/">Consulting & Training</Link></li>
+          <li><Link to="/photobooth/">Magic Mirror AI Photo Booth</Link></li>
+          <li><Link to="/training/">Training & Workshops</Link></li>
         </ul>
       </div>
-      <span className="absolute top-0 left-0 w-full h-full pointer-events-none select-none opacity-20 -z-10 bg-gradient-to-b from-base-100 via-base-100 to-transparent"></span>
-    </div>
+      <span className="absolute top-0 left-0 w-full h-full scale-y-150 pointer-events-none select-none opacity-40 -z-10 bg-gradient-to-b from-base-100 via-base-100 to-transparent"></span>
+    </div >
   )
 };
 
