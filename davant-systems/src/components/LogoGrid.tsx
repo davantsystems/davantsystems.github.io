d99da32be5f6b0bcd9d3499bd51d6f4a235e000a
Grant Keller <grant@kellerdigital.com>
2024-06-08 04:27:32 -0700

    Added more logos

--- davant-systems/src/components/LogoGrid.tsx
+++ davant-systems/src/components/LogoGrid.tsx
@@ -3,116 +3,183 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const LogoGrid = () => {
     return (
-        <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 lg:grid-cols-6">
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-RalphLauren-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="Ralph Lauren logo"
-                />
+        <>
+            <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 lg:grid-cols-4 lg:mb-8">
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-RalphLauren-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="Ralph Lauren logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-AMD-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="AMD logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-UCLA-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-Asus-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
             </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-AMD-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="AMD logo"
-                />
+            <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 lg:grid-cols-5">
+            <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-Hackaday-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="Hackaday logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-VanJones-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="Van Jones logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-TechWeekLA-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-4 sm:px-4 md:px-8 sm:py-2">
+                    <StaticImage
+                        src="../images/logos/logo-silverdraft.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="silverdraft logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-AILA-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="AILA logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-AIMVS-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="AIMVS logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center py-2 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-CuriousRefuge-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="CuriousRefuge logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-GlobalObjects-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="GlobalObjects logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center py-2 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-NFMLA-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="NFMLA logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-XRM-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-BruinAI-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="BruinAI logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center py-2 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-PugetSystems-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-Nuleep-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-FbrcAI-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
+                <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
+                    <StaticImage
+                        src="../images/logos/logo-ImmerseStudio-white.png"
+                        quality={80}
+                        placeholder="blurred"
+                        loading="lazy"
+                        alt="XRM logo"
+                    />
+                </div>
             </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-Hackaday-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="Hackaday logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-VanJones-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="Van Jones logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-AILA-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="AILA logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-AIMVS-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="AIMVS logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-BruinAI-color.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="BruinAI logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-CuriousRefuge-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="CuriousRefuge logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-GlobalObjects-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="GlobalObjects logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-NFMLA-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="NFMLA logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-silverdraft.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="silverdraft logo"
-                />
-            </div>
-            <div className="relative flex items-center justify-center px-2 py-2 sm:px-4 md:px-8 sm:py-0">
-                <StaticImage
-                    src="../images/logos/logo-XRM-white.png"
-                    quality={80}
-                    placeholder="blurred"
-                    loading="lazy"
-                    alt="XRM logo"
-                />
-            </div>
-        </div>
+        </>
     )
 }
 
