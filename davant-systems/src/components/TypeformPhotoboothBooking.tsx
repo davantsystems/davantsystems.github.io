f3a618e625f2a993b821c471eabb86ee71ea8f2c
Grant Keller <grant@kellerdigital.com>
2024-11-15 19:44:42 -0800

    Fix typeform

--- davant-systems/src/components/TypeformPhotoboothBooking.tsx
+++ davant-systems/src/components/TypeformPhotoboothBooking.tsx
@@ -10,7 +10,6 @@ const TypeformPhotoboothBooking = ({ buttonText }: {buttonText: string}) => {
         <PopupButton
             className="mx-auto shadow-lg sm:text-lg text-md md:text-base lg:text-xl text-base-300 shadow-base-300 btn btn-md btn-primary bg-gradient-to-b from-fuchsia-600 via-primary to-fuchsia-900"
             id="gFz9Fc6q"
-            enableSandbox
             transitiveSearchParams>
             {buttonText}
         </PopupButton>
