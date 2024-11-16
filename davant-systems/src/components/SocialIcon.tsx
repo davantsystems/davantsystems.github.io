81a23ee73b39edb48643a1ad5901e727e5905bfb
Grant Keller <grant@kellerdigital.com>
2024-11-15 19:05:15 -0800

    Add hydration check to social icon

--- davant-systems/src/components/SocialIcon.tsx
+++ davant-systems/src/components/SocialIcon.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContentContext from "../ContentContext";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faFacebook, faLinkedin, faYoutube, faGoogle, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 const SocialIcon = ({ contentVariables, platform }: { contentVariables: object, platform: string }) => {
+    const [hydrated, setHydrated] = useState(false);
+
+    useEffect(() => {
+        setHydrated(true);
+    }, []);
+
+    if (!hydrated) return null;
+
     const url = contentVariables.davant.links.social[platform as keyof typeof contentVariables.davant.links.social].url;
     const handle = contentVariables.davant.links.social[platform as keyof typeof contentVariables.davant.links.social].handle;
 
