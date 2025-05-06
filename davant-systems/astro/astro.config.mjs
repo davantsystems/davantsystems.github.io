93137629963b783df72113946ce90596d04d000b
Grant Keller <grant@kellerdigital.com>
2025-05-06 15:32:45 -0700

    Make dirs for old and new site for migration

---
+++ davant-systems/astro/astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Orbitron",
+      cssVariable: "--font-orbitron",
+    }]
+  },
+});
---
+++ davant-systems/astro/tailwind.config.mjs
@@ -0,0 +1,76 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			fontFamily: {
+				'orbitron': ['Orbitron', 'sans-serif'],
+				'yellowtail': ['Yellowtail', 'sans-serif'],
+				'open-sans': ['Open Sans', 'sans-serif'],
+			},
+			fontSize: {
+				xs: ['0.75rem', '1.3'],
+				sm: ['0.875rem', '1.3'],
+				base: ['1rem', '1.5'],
+				lg: ['1.125rem', '1.3'],
+				xl: ['1.125rem', '1.3'],
+				'2xl': ['1.5rem', '1.3'],
+				'3xl': ['1.875rem', '1.3'],
+				'4xl': ['2.25rem', '1.3'],
+			}
+		},
+	},
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("daisyui")
+	],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("daisyui")
+	],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			fontFamily: {
+				'orbitron': ['Orbitron', 'sans-serif'],
+				'yellowtail': ['Yellowtail', 'sans-serif'],
+				'open-sans': ['Open Sans', 'sans-serif'],
+			},
+			fontSize: {
+				xs: ['0.75rem', '1.3'],
+				sm: ['0.875rem', '1.3'],
+				base: ['1rem', '1.5'],
+				lg: ['1.125rem', '1.3'],
+				xl: ['1.125rem', '1.3'],
+				'2xl': ['1.5rem', '1.3'],
+				'3xl': ['1.875rem', '1.3'],
+				'4xl': ['2.25rem', '1.3'],
+			}
+		},
+	},
+	daisyui: {
+		themes: [
+			{
+				synthwave: {
+					...require("daisyui/src/theming/themes")["synthwave"],
+					primary: "hsl(309, 80%, 41%)",
+					secondary: "hsl(187, 90%, 50%)",
+					warning: "hsl(40, 100%, 67%)",
+					"base-100": "hsl(270, 60%, 5%)",
+					"okay": "hsl(120, 100%, 67%)",
+				},
+			},
+		],
+	}
+}
